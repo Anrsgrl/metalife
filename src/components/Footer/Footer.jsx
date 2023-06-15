a2fc1dd487c5377525cc20bb45a348259e25121d
Anrsgrl <esgerlianar003@gmail.com>
2023-06-15 19:56:30 +0400

    Footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import "./Footer.scss";
+import { AiFillFacebook, AiFillInstagram, AiFillTwitterSquare } from "react-icons/ai";
+
 const Footer = () => {
     return (
         <footer>
-            footer
+            <div className="footer-content px-2 container">
+                <span>Copyright 2023 © All Right Reserved</span>
+                <div className="contact-icons">
+                    <button className='clean-button' onClick={() => window.open('https://www.instagram.com/metalife_official_/')}><AiFillFacebook /></button>
+                    <button className='clean-button' onClick={() => window.open('https://www.instagram.com/metalife_official_/')}><AiFillInstagram /></button>
+                    <button className='clean-button' onClick={() => window.open('https://www.instagram.com/metalife_official_/')}><AiFillTwitterSquare /></button>
+                </div>
+            </div>
         </footer>
     )
 }
 
-export default Footer
+export default Footer;
--- src/pages/Home/components/Application/Application.jsx
+++ src/pages/Home/components/Application/Application.jsx
@@ -7,7 +7,7 @@ const Application = () => {
     return (
         <div className="application my-5 container">
             <div className="application-content p-3 h-100">
-                <div className="application-text py-3">
+                <div className="application-text text-center py-3">
                     <h3 className='px-1'>You can get our mobile application</h3>
                     <div className="application-buttons">
                         <button className='application-btn m-2'><AiFillAndroid />Android</button>
