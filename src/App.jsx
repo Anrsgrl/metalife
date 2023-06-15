29d238f892f92aca29a4c9ea17bb75412e3608fb
Anrsgrl <esgerlianar003@gmail.com>
2023-06-15 15:36:42 +0400

    Font added/ header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import "./Header.scss";
+import logo from "../../assets/images/logo3.png";
+import { BiMenuAltLeft } from "react-icons/bi";
+import { VscAccount } from "react-icons/vsc";
 
 const Header = () => {
     return (
         <header>
-            header
+            <div className="container py-2">
+                <div className="header-content">
+                    <button className="hamburger mobile clean-button p-0">
+                        <BiMenuAltLeft />
+                    </button>
+                    <div className="header-left p-0">
+                        <button className="logo-button p-0 clean-button">
+                            <img src={logo} alt="logo" />
+                        </button>
+                        <ul className='desktop m-0 ps-4'>
+                            <li>
+                                <button className="clean-button">Home</button>
+                                <button className="clean-button">About</button>
+                                <button className="clean-button">Lessons</button>
+                                <button className="clean-button">Contact</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="header-profile">
+                        <button className="account mobile clean-button p-0">
+                            <VscAccount />
+                        </button>
+                        <div className="desktop-buttons desktop">
+                            <button className="clean-button">
+                                Lang
+                            </button>
+                            <button className="clean-button">
+                                Sign in
+                            </button>
+                            <button className="clean-button sign-up p-2">
+                                Sign up
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
