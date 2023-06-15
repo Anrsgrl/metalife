2558398cf94119054e3d305e7309f6c00b83c898
Anrsgrl <esgerlianar003@gmail.com>
2023-06-15 16:28:37 +0400

    Hero completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Hero from "../Home/components/Hero/Hero";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Hero />
+        </>
     )
 }
 
--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./Hero.scss";
+import vector from "../../../../assets/images/vector.png";
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className="hero container py-5">
+            <div className="hero-content">
+                <div className="hero-text">
+                    <h1>Metalife</h1>
+                    <p>Can you wish best of dreams</p>
+                    <button class="button-50 mt-3">Get started to learn</button>
+                </div>
+                <div className="hero-vector">
+                    <img src={vector} alt="vector" />
+                </div>
+            </div>
+        </div>
     )
 }
 
