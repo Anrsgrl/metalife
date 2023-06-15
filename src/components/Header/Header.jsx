e70e2d6ef1654f583e644c7c65c64062c45ed209
Anrsgrl <esgerlianar003@gmail.com>
2023-06-15 13:03:56 +0400

    Structure updated/ packages have installed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <header>
+            header
+        </header>
+    )
+}
+
+export default Header
