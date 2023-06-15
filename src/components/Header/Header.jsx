0b1a14ed208abbac40e5cbc96ebcf8a2f3a42bbb
Anrsgrl <esgerlianar003@gmail.com>
2023-06-15 20:14:14 +0400

    header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.scss";
 import logo from "../../assets/images/logo3.png";
 import { BiMenuAltLeft } from "react-icons/bi";
 import { VscAccount } from "react-icons/vsc";
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -16,13 +17,11 @@ const Header = () => {
                         <button className="logo-button p-0 clean-button">
                             <img src={logo} alt="logo" />
                         </button>
-                        <ul className='desktop m-0 ps-4'>
-                            <li>
-                                <button className="clean-button">Home</button>
-                                <button className="clean-button">About</button>
-                                <button className="clean-button">Lessons</button>
-                                <button className="clean-button">Contact</button>
-                            </li>
+                        <ul className='header-menu desktop m-0 ps-4'>
+                            <li><NavLink to="/">Home</NavLink></li>
+                            <li><NavLink to="/about">About</NavLink></li>
+                            <li><NavLink to="/lessons">Lessons</NavLink></li>
+                            <li><NavLink to="/contact">Contact</NavLink></li>
                         </ul>
                     </div>
                     <div className="header-profile">
@@ -30,10 +29,12 @@ const Header = () => {
                             <VscAccount />
                         </button>
                         <div className="desktop-buttons desktop">
-                            <button className="clean-button">
-                                Lang
-                            </button>
-                            <button className="clean-button">
+                            <select name="sort-list" id="sort-list">
+                                <option value="en">EN</option>
+                                <option value="aze">AZE</option>
+                                <option value="ru">RU</option>
+                            </select>
+                            <button className="sign-in px-3 clean-button">
                                 Sign in
                             </button>
                             <button className="clean-button sign-up p-2">
