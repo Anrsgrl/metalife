1089505086058efe99b9efa5c776ba4426f08bec
Anrsgrl <esgerlianar003@gmail.com>
2023-06-21 18:56:46 +0400

    Nav search-bar fixed removed/ hashtags.js added

---
+++ src/components/Nav/Hashtags.js
@@ -0,0 +1,3 @@
+export const hashtags = [
+    "html", "css", "scss", "javascript", "react", "java", "mongodb", "expressjs", "figma", "adobexd", "autodesk", "turbocad"
+]
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,40 +1,20 @@
 import React from 'react';
 import { BiSearchAlt } from "react-icons/bi";
 import "./Nav.scss";
+import { hashtags } from './Hashtags';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 const Nav = () => {
-
-    const [fixed, setFixed] = useState(false);
-    const changeColor = () => {
-        if (window.scrollY >= 96) {
-            setFixed(true);
-        } else {
-            setFixed(false);
-        }
-    };
-
-    window.addEventListener('scroll', changeColor);
     return (
         <div className="navv p-0 container">
-            <div className={fixed ? "fixed-search-bar search-bar" : "relative-search-bar search-bar"}>
+            <div className="search-bar">
                 <input type="text" placeholder='Search...' name="" id="" className='ps-2 pe-4' />
                 <button type="submit"><BiSearchAlt /></button>
             </div>
             <ul className='px-0 pt-1 m-0 hashtags'>
-                <li><Link className='text-muted' to="/">#html</Link></li>
-                <li><Link className='text-muted' to="/">#css</Link></li>
-                <li><Link className='text-muted' to="/">#scss</Link></li>
-                <li><Link className='text-muted' to="/">#javascript</Link></li>
-                <li><Link className='text-muted' to="/">#react</Link></li>
-                <li><Link className='text-muted' to="/">#java</Link></li>
-                <li><Link className='text-muted' to="/">#mongodb</Link></li>
-                <li><Link className='text-muted' to="/">#expressjs</Link></li>
-                <li><Link className='text-muted' to="/">#figma</Link></li>
-                <li><Link className='text-muted' to="/">#adobexd</Link></li>
-                <li><Link className='text-muted' to="/">#autodesk</Link></li>
-                <li><Link className='text-muted' to="/">#turbocad</Link></li>
+                {hashtags.map((hashtag) => (
+                    <li><Link className='text-muted' to="/">#{hashtag}</Link></li>
+                ))}
             </ul>
         </div>
     )
