5c1c30cbf5c3d850b10929fe169b7db3aba79048
Anrsgrl <esgerlianar003@gmail.com>
2023-06-22 00:11:13 +0400

    Stuff updated

--- src/pages/Home/components/Stuff/Stuff.jsx
+++ src/pages/Home/components/Stuff/Stuff.jsx
@@ -1,57 +1,26 @@
 import React from 'react';
 import "./Stuff.scss";
-import boy from "../../../../assets/images/boy.png";
-import girl from "../../../../assets/images/girl.png";
 import StuffItem from './StuffItem';
+import { stuffList } from './StuffList';
 
 const Stuff = () => {
     return (
         <div className="stuff container pt-3 pb-5">
             <div className="stuff-content row">
                 <h2 data-aos="fade-up" className='section-heading py-3'>Our team</h2>
-                <StuffItem
-                    name="Nazir Nadirov"
-                    position="Founder, Director"
-                    image={boy}
-                />
-                <StuffItem
-                    name="Elvin Namazov"
-                    position="Marketing Manager"
-                    image={boy}
-                />
-                <StuffItem
-                    name="Seymur Seferov"
-                    position="Human Resource"
-                    image={boy}
-                />
-                <StuffItem
-                    name="Anar Asgarli"
-                    position="Frontend Developer"
-                    image={boy}
-                    github="https://github.com/Anrsgrl"
-                />
-                <StuffItem
-                    name="Lala Asgarova"
-                    position="Frontend Developer"
-                    image={girl}
-                    github="https://github.com/lolaaskerova"
-                />
-                <StuffItem
-                    name="Aynura Qaniyeva"
-                    position="Frontend Dev Teacher"
-                    image={girl}
-                    github="https://github.com/aynura12"
-                />
-                <StuffItem
-                    name="Qeybulla"
-                    position="Backend Dev"
-                    image={boy}
-                />
-                <StuffItem
-                    name="Fuad Beybutov"
-                    position="Designer"
-                    image={boy}
-                />
+                {stuffList.map((item) => (
+                    <StuffItem
+                        key={item.id}
+                        name={item.name}
+                        position={item.position}
+                        image={item.image}
+                        github={item.github}
+                        linkedin={item.linkedin}
+                        behance={item.behance}
+                        instagram={item.instagram}
+                        twitter={item.twitter}
+                    />
+                ))}
             </div>
         </div>
     )
---
+++ src/pages/Home/components/Stuff/StuffList.js
@@ -0,0 +1,57 @@
+import boy from "../../../../assets/images/boy.png";
+import girl from "../../../../assets/images/girl.png";
+
+
+export const stuffList = [
+    {
+        id: "1",
+        name: "Nazir Nadirov",
+        position: "Founder, Director",
+        image: boy,
+    },
+    {
+        id: "2",
+        name: "Elvin Namazov",
+        position: "Marketing Manager",
+        image: boy,
+    },
+    {
+        id: "3",
+        name: "Seymur Seferov",
+        position: "Human Resource",
+        image: boy,
+    },
+    {
+        id: "4",
+        name: "Anar Asgarli",
+        position: "Frontend Developer",
+        image: boy,
+        github: "https://github.com/Anrsgrl",
+    },
+    {
+        id: "5",
+        name: "Lala Asgarova",
+        position: "Frontend Developer",
+        image: girl,
+        github: "https://github.com/lolaaskerova",
+    },
+    {
+        id: "6",
+        name: "Aynura Qaniyeva",
+        position: "Frontend Dev Teacher",
+        image: girl,
+        github: "https://github.com/aynura12",
+    },
+    {
+        id: "7",
+        name: "Qeybulla",
+        position: "Backend Dev",
+        image: boy,
+    },
+    {
+        id: "8",
+        name: "Fuad Beybutov",
+        position: "Designer",
+        image: boy,
+    },
+]
