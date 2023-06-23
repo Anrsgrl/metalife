d65bade6aab806179d402716387844ee72c6884f
Anrsgrl <esgerlianar003@gmail.com>
2023-06-23 10:38:30 +0400

    fade up added to portfolio items

--- src/pages/Portfolio/PortfolioItem.jsx
+++ src/pages/Portfolio/PortfolioItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PortfolioItem = (props) => {
     const { image } = props;
     return (
-        <div className="portfolio-website skeleton-loader col-12 col-md-6">
+        <div data-aos="fade-up" className="portfolio-website skeleton-loader col-12 col-md-6">
             <img className='w-100 h-100' src={image} alt="website" />
         </div>
     )
