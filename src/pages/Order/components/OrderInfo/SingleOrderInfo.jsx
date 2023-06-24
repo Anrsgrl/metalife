4d3ea88f9dc365a1e432adc93cd4fc4afca139ca
Anrsgrl <esgerlianar003@gmail.com>
2023-06-24 21:12:02 +0400

    Aos added to order

--- src/pages/Order/components/OrderInfo/SingleOrderInfo.jsx
+++ src/pages/Order/components/OrderInfo/SingleOrderInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SingleOrderInfo = (props) => {
     const { svg, title, content, smth } = props;
     return (
-        <div className="single-order-info pb-2 col-12 col-md-6 col-lg-4">
+        <div data-aos="fade-right" className="single-order-info pb-2 col-12 col-md-6 col-lg-4">
             {svg}
             <h4>{title}</h4>
             <p>{content}</p>
--- src/pages/Order/components/OrderPrice/OrderPrice.jsx
+++ src/pages/Order/components/OrderPrice/OrderPrice.jsx
@@ -8,15 +8,15 @@ const OrderPrice = () => {
     const [change, setChange] = useState(false);
     return (
         <div className="order-price container py-3">
-            <h2 className='py-4'>Qiymət təklifinin alınması</h2>
+            <h2 data-aos="fade-up" className='py-4'>Qiymət təklifinin alınması</h2>
             <form action="">
                 <div className="row">
-                    <div className="form-element col-12">
+                    <div data-aos="fade-right" className="form-element col-12">
                         <textarea placeholder='Necə bir sayt istəyirsiniz?' className='sign-text-area' name="describe" cols="30" rows="10"></textarea>
                     </div>
-                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Adınız' name="name" className='sign-inputs' required /></div>
-                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Şirkətin adı' name="company" className='sign-inputs' required /></div>
-                    {change ? (<div className="form-element col-12 col-md-6">
+                    <div data-aos="fade-right" className="form-element col-12 col-md-6"><input type="text" placeholder='Adınız' name="name" className='sign-inputs' required /></div>
+                    <div data-aos="fade-right" className="form-element col-12 col-md-6"><input type="text" placeholder='Şirkətin adı' name="company" className='sign-inputs' required /></div>
+                    {change ? (<div data-aos="fade-right" className="form-element col-12 col-md-6">
                         <InputMask
                             className='sign-inputs'
                             mask="+999\-99-999-99-99"
@@ -26,11 +26,11 @@ const OrderPrice = () => {
                             required
                         ></InputMask>
                         <button onClick={() => setChange(!change)} type='button' className='clean-button btn-show'>{change ? (<TfiReload />) : (<TfiReload />)}</button>
-                    </div>) : (<div className="form-element col-12 col-md-6">
+                    </div>) : (<div data-aos="fade-right" className="form-element col-12 col-md-6">
                         <div className="form-element col-12"><input type="email" placeholder='E-poçt' name="email" className='sign-inputs' required /></div>
                         <button onClick={() => setChange(!change)} type='button' className='clean-button btn-show'>{change ? (<TfiReload />) : (<TfiReload />)}</button>
                     </div>)}
-                    <div className="col-md-6 form-element">
+                    <div data-aos="fade-right" className="col-md-6 form-element">
                         <button className='btn-blue sign-inputs'>Qiymət təklifi al</button>
                     </div>
                 </div>
