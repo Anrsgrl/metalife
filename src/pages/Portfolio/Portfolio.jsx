52aeeb22c959ac453a87859cdabbd6f049953b23
Anrsgrl <esgerlianar003@gmail.com>
2023-06-24 19:03:26 +0400

    ScrollToTop added to order and contact

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import "./Portfolio.scss";
 import PortfolioItem from './PortfolioItem';
 import { PortfolioItems } from './PortfolioItems';
+import { useEffect } from 'react';
 
 const Portfolio = () => {
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: "auto",
+        });
+    }, []);
     return (
         <div className="portfolio py-5 container">
             <div className="row">
