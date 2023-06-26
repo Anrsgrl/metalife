ee40e00d19bbc9d04c8567cae3988313cea64798
Anrsgrl <esgerlianar003@gmail.com>
2023-06-26 14:16:10 +0400

    LessonList updated

--- src/data/LessonList.js
+++ src/data/LessonList.js
@@ -10,7 +10,7 @@ export const lessonList = [
     {
         id: "1",
         title: "Fullstack developer",
-        shortDescribe: "Html, Css, Javascript, React, MongoDB, Java and ExpressJS",
+        shortDescribe: "Html, Css, Javascript, React, MongoDB, Java & ExpressJS",
         describe: "Siz bu kurs zamanı ilk olaraq HTML5, CSS3 (əlavə olaraq SCSS və Bootstrap) və Javascripti keçəcəksiniz. Daha sonra isə Javacscriptin frameworkü olan ReactJS ilə dərslərə davam edəcəksiniz. ReactJS müəyyən bir levelə çatdıqdan sonra siz ExpressJS, NodeJS və MongoDB öyrənəcəksiniz. Daha sonra isə müxtəlif proyektlər ilə full stack applicationlar yaradaraq bu biliklərinizi təkminləşdirəcəksiniz. Dərslər bitdikdə artıq siz sərbəst şəkildə fullstack proyektlər hazırlaya biləcəksiniz!",
         image: fullstack,
         path: "fullstack",
@@ -18,7 +18,7 @@ export const lessonList = [
     {
         id: "2",
         title: "Frontend developer",
-        shortDescribe: "Html, Css, Javascript and React",
+        shortDescribe: "Html, Css, Javascript & React",
         describe: "Siz bu kurs zamanı ilk olaraq HTML5, CSS3 (əlavə olaraq SCSS və Bootstrap) və Javascripti keçəcəksiniz. Daha sonra isə Javacscriptin frameworkü olan ReactJS ilə dərslərə davam edəcəksiniz. Dərslər bitdikdə artıq siz istənilən websaytın interfeysini yığa biləcəksiniz!",
         image: frontend,
         path: "frontend",
@@ -26,15 +26,15 @@ export const lessonList = [
     {
         id: "3",
         title: "Backend developer",
-        shortDescribe: "Java və MySQL",
-        describe: "Siz bu kurs zamanı Java və MySQL-i irəli səviyəyə qədər öyrənəcəksiniz. Kurs bitdikdə artıq siz işləməyə hazır olacaqsınız!",
+        shortDescribe: "Java & MySQL",
+        describe: "Siz bu kurs zamanı Java & MySQL-i irəli səviyəyə qədər öyrənəcəksiniz. Kurs bitdikdə artıq siz işləməyə hazır olacaqsınız!",
         image: backend,
         path: "backend",
     },
     {
         id: "4",
         title: "UI/UIX Dizayner",
-        shortDescribe: "Figma and AdobeXD",
+        shortDescribe: "Figma & AdobeXD",
         describe: "Siz bu kurs zamanı Figma və AdobeXD programlarında istənilən dizaynı, tam sərbəst işləməyi öyrənəcəksiniz!",
         image: ui,
         path: "ui-designer",
@@ -42,7 +42,7 @@ export const lessonList = [
     {
         id: "5",
         title: "Interyer Dizayner",
-        shortDescribe: "Autodesk, SketchUp Pro and TurboCAD",
+        shortDescribe: "Autodesk, SketchUp Pro & TurboCAD",
         describe: "Siz bu kurs zamanı AutoDesk, SketchUp və TurboCAD kimi müsair İnteryer Dizayner programlarında işləməyi öyrənəcəksiniz!",
         image: design,
         path: "interior-designer",
@@ -50,7 +50,7 @@ export const lessonList = [
     {
         id: "6",
         title: "3D Modelləmə",
-        shortDescribe: "Blender and Cinema4D",
+        shortDescribe: "Blender & Cinema4D",
         describe: "Siz bu kurs zamanı Blender və Cinema4D programlarıyla işləməyi öyrənəcək, tam sərbəst şəkildə 3d layihələr işləyə biləcəksiniz!",
         image: d,
         path: "3d-modelling",
