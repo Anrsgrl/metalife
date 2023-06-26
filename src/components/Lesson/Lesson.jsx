96e3b6f55968d7b8963d420bba1326e52e08f487
Anrsgrl <esgerlianar003@gmail.com>
2023-06-26 20:24:12 +0400

    Changes on single lesson

--- src/components/Lesson/Lesson.jsx
+++ src/components/Lesson/Lesson.jsx
@@ -6,7 +6,7 @@ const Lesson = (props) => {
     const { image, title, p, path } = props;
     const navigate = useNavigate()
     return (
-        <div data-aos="fade-up" className="col-9 col-md-5 col-lg-3 skeleton-loader me-0 me-md-4 mb-4 p-0 lesson">
+        <button onClick={() => navigate(`/lessons/${path}`)} data-aos="fade-up" type='button' className="col-9 col-md-5 col-lg-3 skeleton-loader clean-button me-0 me-md-4 mb-4 p-0 lesson">
             <img src={image} alt="design" />
             <div className="lesson-text px-2">
                 <h3>{title}</h3>
@@ -15,9 +15,9 @@ const Lesson = (props) => {
                 </p>
             </div>
             <div className="lesson-footer">
-                <button onClick={() => navigate(`/lessons/${path}`)} className='clean-button p-3'>Daha çox <AiOutlineRight /></button>
+                <button type='button' className='clean-button p-3'>Daha çox <AiOutlineRight /></button>
             </div>
-        </div>
+        </button>
     )
 }
 
--- src/pages/SingleLesson/components/SingleLessonBlogs/SingleLessonBlogs.jsx
+++ src/pages/SingleLesson/components/SingleLessonBlogs/SingleLessonBlogs.jsx
@@ -4,9 +4,9 @@ import SingleBlog from '../../../../components/SingleBlog/SingleBlog';
 
 const SingleLessonBlogs = () => {
     return (
-        <div className="single-lesson-blogs py-3">
+        <div className="single-lesson-blogs py-5">
             <h3 className='py-1'>Bu mövzu barədə bəzi bloglar</h3>
-            <div className="lesson-blogs pt-1 pb-2  ">
+            <div className="lesson-blogs py-3">
                 <SingleBlog title="Http və Https arasındaki fərqliliklər nədir?" image="https://ithemes.com/wp-content/uploads/2022/11/HTTP-vs-HTTPS.png" />
                 <SingleBlog title="CSS3 yoxsa SCSS?" image="https://codersera.com/blog/wp-content/uploads/2020/04/css_vs_scss.jpg" />
                 <SingleBlog title="CSS3 yoxsa SCSS?" image="https://codersera.com/blog/wp-content/uploads/2020/04/css_vs_scss.jpg" />
