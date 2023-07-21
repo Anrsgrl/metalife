bccc713584b9bb51dbc7acae0c5a80ebcab62045
Anrsgrl <esgerlianar003@gmail.com>
2023-07-21 21:43:24 +0400

    Email process added

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useEffect } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 const Contact = () => {
+    const form = useRef();
+    const [status, setStatus] = useState(false)
+    const [error, setError] = useState("")
     useEffect(() => {
         window.scrollTo({
             top: 0,
             behavior: "auto",
         });
     }, []);
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(process.env.REACT_APP_EMAIL_SERVICE_ID, "template_8vp460o", form.current, process.env.REACT_APP_PUBLIC_KEY)
+            .then(() => {
+                setStatus(true);
+                setError(false);
+                form.current.reset()
+            })
+            .catch((error) => {
+                setStatus(false);
+                setError(true);
+                console.error("Xəta baş verdi:", error.message);
+            });
+    };
     return (
         <div className="contact container py-5">
-            <form className='py-4' action="">
+            <form onSubmit={(e) => sendEmail(e)} ref={form} className='py-4'>
                 <h3 className='py-2'>Əlaqə</h3>
                 <div className="row">
-                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Ad' name="name" className='sign-inputs' required /></div>
-                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Soyad' name="surname" className='sign-inputs' required /></div>
-                    <div className="form-element col-12 col-md-6"><input type="email" placeholder='E-poçt' name="email" className='sign-inputs' required /></div>
-                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Mövzu' name="subject" className='sign-inputs' required /></div>
+                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Ad' name="from_name" className='sign-inputs' required /></div>
+                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Soyad' name="from_surname" className='sign-inputs' /></div>
+                    <div className="form-element col-12 col-md-6"><input type="email" placeholder='E-poçt' name="from_mail" className='sign-inputs' required /></div>
+                    <div className="form-element col-12 col-md-6"><input type="text" placeholder='Mövzu' name="from_subject" className='sign-inputs' required /></div>
                     <div className="form-element col-12">
-                        <textarea placeholder='Mesajınız' className='sign-text-area' name="comment" cols="30" rows="10"></textarea>
+                        <textarea placeholder='Mesajınız' className='sign-text-area' name="message" cols="30" rows="10"></textarea>
                     </div>
                 </div>
                 <div className="sign-buttons">
                     <button className='btn-blue'>E-poçt göndər</button>
                 </div>
+                {status && <p className='text-success py-2'>E-poçt göndərildi!</p>}
+                {error && <p className='text-danger'>Xəta baş verdi! Zəhmət olmasa daha sonra təzdən yoxlayın.</p>}
             </form>
         </div>
     )
--- src/pages/Order/components/OrderPrice/OrderPrice.jsx
+++ src/pages/Order/components/OrderPrice/OrderPrice.jsx
@@ -1,39 +1,97 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import InputMask from 'react-input-mask'
 import { useState } from 'react';
 import { TfiReload } from "react-icons/tfi";
+import emailjs from '@emailjs/browser';
 
 const OrderPrice = () => {
+    const form = useRef();
     const [tel, setTel] = useState();
     const [change, setChange] = useState(false);
+    const [status, setStatus] = useState(false);
+    const [error, setError] = useState("");
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(process.env.REACT_APP_EMAIL_SERVICE_ID, "template_7cs7jeg", form?.current, process.env.REACT_APP_PUBLIC_KEY)
+            .then(() => {
+                setStatus(true);
+                setError(false);
+                form?.current?.reset();
+                setTel("")
+            })
+            .catch((error) => {
+                setStatus(false);
+                setError(true);
+                console.error("Xəta baş verdi:", error.message);
+            });
+    };
+
+
     return (
         <div className="order-price container py-3">
             <h2 data-aos="fade-up" className='py-4'>Qiymət təklifinin alınması</h2>
-            <form action="">
+            <form ref={form} onSubmit={(e) => sendEmail(e)}>
                 <div className="row">
                     <div data-aos="fade-right" className="form-element col-12">
                         <textarea placeholder='Necə bir sayt istəyirsiniz?' className='sign-text-area' name="describe" cols="30" rows="10"></textarea>
                     </div>
-                    <div data-aos="fade-right" className="form-element col-12 col-md-6"><input type="text" placeholder='Adınız' name="name" className='sign-inputs' required /></div>
-                    <div data-aos="fade-right" className="form-element col-12 col-md-6"><input type="text" placeholder='Şirkətin adı' name="company" className='sign-inputs' required /></div>
-                    {change ? (<div data-aos="fade-right" className="form-element col-12 col-md-6">
-                        <InputMask
-                            className='sign-inputs'
-                            mask="+999\-99-999-99-99"
-                            placeholder="+XXX-XX-XXX-XX-XX"
-                            value={tel || ''}
-                            onChange={(e) => setTel(e.target.value)}
-                            required
-                        ></InputMask>
-                        <button onClick={() => setChange(!change)} type='button' className='clean-button btn-show'>{change ? (<TfiReload className={change && "rotate-180"} />) : (<TfiReload />)}</button>
-                    </div>) : (<div data-aos="fade-right" className="form-element col-12 col-md-6">
-                        <div className="form-element col-12"><input type="email" placeholder='E-poçt' name="email" className='sign-inputs' required /></div>
-                        <button onClick={() => setChange(!change)} type='button' className='clean-button btn-show'>{change ? (<TfiReload />) : (<TfiReload />)}</button>
-                    </div>)}
+                    <div data-aos="fade-right" className="form-element col-12 col-md-6">
+                        <input type="text" placeholder='Adınız' name="from_name" className='sign-inputs' required /></div>
+                    <div data-aos="fade-right" className="form-element col-12 col-md-6">
+                        <input type="text" placeholder='Şirkətin adı' name="from_company" className='sign-inputs' required />
+                    </div>
+                    <div data-aos="fade-right" className="form-element col-12 col-md-6">
+                        {change ? (
+                            <>
+                                <InputMask
+                                    name="from_number"
+                                    className="sign-inputs"
+                                    mask="+999\-99-999-99-99"
+                                    placeholder="+XXX-XX-XXX-XX-XX"
+                                    value={tel || ""}
+                                    onChange={(e) => setTel(e.target.value)}
+                                    required
+                                />
+                                <button
+                                    onClick={() => setChange(!change)}
+                                    type="button"
+                                    className="clean-button btn-show"
+                                >
+                                    {change ? (
+                                        <TfiReload className={change && "rotate-180"} />
+                                    ) : (
+                                        <TfiReload />
+                                    )}
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <input
+                                    type="email"
+                                    placeholder="E-poçt"
+                                    name="from_mail"
+                                    className="sign-inputs"
+                                    required
+                                />
+                                <button
+                                    onClick={() => setChange(!change)}
+                                    type="button"
+                                    className="clean-button btn-show"
+                                >
+                                    {change ? <TfiReload /> : <TfiReload />}
+                                </button>
+                            </>
+                        )}
+                    </div>
+
                     <div data-aos="fade-right" className="col-md-6 form-element">
                         <button className='btn-blue sign-inputs'>Qiymət təklifi al</button>
                     </div>
                 </div>
+                {status && <p className='text-success py-2'>E-poçt göndərildi! Yaxın zamanda sizinlə əlaqə saxlanılacaq!</p>}
+                {error && <p className='text-danger'>Xəta baş verdi! Zəhmət olmasa daha sonra təzdən yoxlayın.</p>}
             </form>
         </div>
     )
