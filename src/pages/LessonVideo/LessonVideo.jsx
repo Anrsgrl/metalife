4384ec1d1ad86fe9fd6ef0e8434538295afb88fc
Anrsgrl <esgerlianar003@gmail.com>
2023-07-23 13:21:54 +0400

    LessonVideos updated

--- src/pages/LessonVideo/LessonVideo.jsx
+++ src/pages/LessonVideo/LessonVideo.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./LessonVideo.scss";
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { TiArrowBack } from "react-icons/ti";
-import ReactPlayer from 'react-player';
 import { useAuth } from "../../firebase";
 import YouTube from 'react-youtube';
+import { BiTime } from "react-icons/bi";
 
 const LessonVideo = () => {
     const params = useParams();
     const { currentUser, videos } = useAuth();
+    const [videoDuration, setVideoDuration] = useState(null);
     const navigate = useNavigate();
+
+    const onPlayerReady = (event) => {
+        const player = event.target;
+        const duration = player.getDuration();
+        setVideoDuration(duration);
+    };
+
+    const formatTime = (totalSeconds) => {
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = Math.floor(totalSeconds % 60);
+        return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    };
 
     const paidVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "false");
     const demoVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "true");
@@ -18,15 +32,18 @@ const LessonVideo = () => {
             <button onClick={() => navigate(-1)} type="button" className='clean-button back'><TiArrowBack /></button>
             <div className="demo-videos-field py-2">
                 <h3 className='pb-2'>Pulsuz videolar</h3>
-                <div className="demo-videos py-3">
+                <div className="demo-videos py-3 h-100 w-100">
                     {currentUser ? (
                         <>
                             {demoVideos.length === 0 && <h5 className='py-2'>Yaxın zamanda əlavə olunacaq...</h5>}
                             {demoVideos &&
                                 demoVideos?.map((e) => (
-                                    <div className='single-video-part'>
-                                        <YouTube videoId={e.url} />
-                                        <h6 className='pt-3'>{e.title}</h6>
+                                    <div className='single-video-part col-12 col-md-9 col-lg-6'>
+                                        <YouTube videoId={e.url} className={"w-100 h-100"} iframeClassName={"w-100 h-100"} onReady={onPlayerReady} />
+                                        <div className="video-info px-3 py-2">
+                                            <h6>{e.title}</h6>
+                                            {videoDuration !== null && <p className='video-time'><BiTime /> {formatTime(videoDuration)}</p>}
+                                        </div>
                                     </div>
                                 ))
                             }
@@ -44,11 +61,13 @@ const LessonVideo = () => {
                 </div>}
                 {paidVideos && paidVideos.map((e) => (
                     <>
-                        <ReactPlayer key={e.id} url={e.url} controls={true} className="single-video" config={{
-                            playerOptions: {
-                                playsinline: true
-                            }
-                        }} />
+                        <div className='single-video-part col-12 col-md-9 col-lg-6'>
+                            <YouTube videoId={e.url} className={"w-100 h-100"} iframeClassName={"w-100 h-100"} onReady={onPlayerReady} />
+                            <div className="video-info px-3 py-2">
+                                <h6>{e.title}</h6>
+                                {videoDuration !== null && <p className='video-time'><BiTime /> {formatTime(videoDuration)}</p>}
+                            </div>
+                        </div>
                     </>
                 ))}
             </div>
