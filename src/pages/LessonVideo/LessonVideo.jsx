196dcb0dc4735a615ec577f030aa59984d291b24
Anrsgrl <esgerlianar003@gmail.com>
2023-07-23 15:22:20 +0400

    UserProducts fixed

--- src/pages/LessonVideo/LessonVideo.jsx
+++ src/pages/LessonVideo/LessonVideo.jsx
@@ -8,7 +8,7 @@ import { BiTime } from "react-icons/bi";
 
 const LessonVideo = () => {
     const params = useParams();
-    const { currentUser, videos } = useAuth();
+    const { currentUser, loggedUser, videos } = useAuth();
     const [videoDuration, setVideoDuration] = useState(null);
     const navigate = useNavigate();
 
@@ -27,9 +27,28 @@ const LessonVideo = () => {
 
     const paidVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "false");
     const demoVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "true");
+    const groupVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "false" && e?.group === loggedUser?.lessons[0]);
     return (
         <div className="lesson-videos container py-3">
             <button onClick={() => navigate(-1)} type="button" className='clean-button back'><TiArrowBack /></button>
+            {groupVideos.length !== 0 &&
+                <div className="group-videos py-5">
+                    <h3 className='pb-2'>{loggedUser?.lessons[0]}</h3>
+                    <>
+                        {groupVideos.length === 0 && <h5 className='py-2'>Yaxın zamanda əlavə olunacaq...</h5>}
+                        {groupVideos &&
+                            groupVideos?.map((e) => (
+                                <div className='single-video-part col-12 col-md-9 col-lg-6'>
+                                    <YouTube videoId={e.url} className={"w-100 h-100"} iframeClassName={"w-100 h-100"} onReady={onPlayerReady} />
+                                    <div className="video-info px-3 py-2">
+                                        <h6>{e.title}</h6>
+                                        {videoDuration !== null && <p className='video-time'><BiTime /> {formatTime(videoDuration)}</p>}
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </>
+                </div>}
             <div className="demo-videos-field py-2">
                 <h3 className='pb-2'>Pulsuz videolar</h3>
                 <div className="demo-videos py-3 h-100 w-100">
