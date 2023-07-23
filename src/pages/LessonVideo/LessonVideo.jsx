ab4a4f0868091ab3a880ed7e891d5b59659779b8
Anrsgrl <esgerlianar003@gmail.com>
2023-07-23 15:40:25 +0400

    LessonVideo fixed

--- src/pages/LessonVideo/LessonVideo.jsx
+++ src/pages/LessonVideo/LessonVideo.jsx
@@ -25,33 +25,35 @@ const LessonVideo = () => {
         return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     };
 
-    const paidVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "false");
+    const paidVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "false" && e?.paid === "paid");
     const demoVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "true");
     const groupVideos = videos.filter((e) => e.hashtags?.includes(params.lessonPath) && e.demo === "false" && e?.group === loggedUser?.lessons[0]);
     return (
         <div className="lesson-videos container py-3">
             <button onClick={() => navigate(-1)} type="button" className='clean-button back'><TiArrowBack /></button>
             {groupVideos.length !== 0 &&
-                <div className="group-videos py-5">
+                <div className="videos-field">
                     <h3 className='pb-2'>{loggedUser?.lessons[0]}</h3>
-                    <>
-                        {groupVideos.length === 0 && <h5 className='py-2'>Yaxın zamanda əlavə olunacaq...</h5>}
-                        {groupVideos &&
-                            groupVideos?.map((e) => (
-                                <div className='single-video-part col-12 col-md-9 col-lg-6'>
-                                    <YouTube videoId={e.url} className={"w-100 h-100"} iframeClassName={"w-100 h-100"} onReady={onPlayerReady} />
-                                    <div className="video-info px-3 py-2">
-                                        <h6>{e.title}</h6>
-                                        {videoDuration !== null && <p className='video-time'><BiTime /> {formatTime(videoDuration)}</p>}
+                    <div className="videos-list py-3">
+                        <>
+                            {groupVideos.length === 0 && <h5 className='py-2'>Yaxın zamanda əlavə olunacaq...</h5>}
+                            {groupVideos &&
+                                groupVideos?.map((e) => (
+                                    <div className='single-video-part col-12 col-md-9 col-lg-6'>
+                                        <YouTube videoId={e.url} className={"w-100 h-100"} iframeClassName={"w-100 h-100"} onReady={onPlayerReady} />
+                                        <div className="video-info px-3 py-2">
+                                            <h6>{e.title}</h6>
+                                            {videoDuration !== null && <p className='video-time'><BiTime /> {formatTime(videoDuration)}</p>}
+                                        </div>
                                     </div>
-                                </div>
-                            ))
-                        }
-                    </>
+                                ))
+                            }
+                        </>
+                    </div>
                 </div>}
-            <div className="demo-videos-field py-2">
+            <div className="videos-field">
                 <h3 className='pb-2'>Pulsuz videolar</h3>
-                <div className="demo-videos py-3 h-100 w-100">
+                <div className="videos-list">
                     {currentUser ? (
                         <>
                             {demoVideos.length === 0 && <h5 className='py-2'>Yaxın zamanda əlavə olunacaq...</h5>}
@@ -72,23 +74,25 @@ const LessonVideo = () => {
                     )}
                 </div>
             </div>
-            <div className="paid-videos pt-5">
+            <div className="videos-field">
                 <h3 className='pb-2'>Ödənişli videolar</h3>
                 {paidVideos.length === 0 && <div className="paid-video py-2">
                     {currentUser?.emailVerified
                         ? (<><h5>Ödənişli dərsləri almaq üçün</h5><button type="button" className='btn-blue'>Dərsi al</button></>) : (<p>Dərsləri almaq üçün giriş etməli və emailin sizin olduğunu profil hissəsindən təstiqləməlisiniz.</p>)}
                 </div>}
-                {paidVideos && paidVideos.map((e) => (
-                    <>
-                        <div className='single-video-part col-12 col-md-9 col-lg-6'>
-                            <YouTube videoId={e.url} className={"w-100 h-100"} iframeClassName={"w-100 h-100"} onReady={onPlayerReady} />
-                            <div className="video-info px-3 py-2">
-                                <h6>{e.title}</h6>
-                                {videoDuration !== null && <p className='video-time'><BiTime /> {formatTime(videoDuration)}</p>}
+                <div className="videos-list">
+                    {paidVideos && paidVideos.map((e) => (
+                        <>
+                            <div className='single-video-part col-12 col-md-9 col-lg-6'>
+                                <YouTube videoId={e.url} className={"w-100 h-100"} iframeClassName={"w-100 h-100"} onReady={onPlayerReady} />
+                                <div className="video-info px-3 py-2">
+                                    <h6>{e.title}</h6>
+                                    {videoDuration !== null && <p className='video-time'><BiTime /> {formatTime(videoDuration)}</p>}
+                                </div>
                             </div>
-                        </div>
-                    </>
-                ))}
+                        </>
+                    ))}
+                </div>
             </div>
         </div>
     )
