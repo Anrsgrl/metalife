99211f6556e301caae5e6466ec3646ede48ed55e
Anrsgrl <esgerlianar003@gmail.com>
2023-08-16 14:28:33 +0400

    Unlocked

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.css';
-// import Header from "./components/Header/Header";
-// import Footer from "./components/Footer/Footer";
-// import DragButton from "./components/DragButton/DragButton";
-// import Home from "./pages/Home/Home";
-// import LessonsPage from "./pages/LessonsPage/LessonsPage";
-// import SingleLesson from "./pages/SingleLesson/SingleLesson";
-// import LessonVideo from "./pages/LessonVideo/LessonVideo";
-// import Portfolio from "./pages/Portfolio/Portfolio";
-// import Order from "./pages/Order/Order";
-// import Blogs from "./pages/Blogs/Blogs";
-// import BlogDetail from "./pages/Blogs/components/BlogDetail/BlogDetail";
-// import SignUp from "./pages/SignUp/SignUp";
-// import SignIn from "./pages/SignIn/SignIn";
-// import Contact from "./pages/Contact/Contact";
-// import User from "./pages/User/User";
-// import Settings from "./pages/Settings/Settings";
-// import Error from "./pages/Error/Error";
-// import Search from "./pages/Search/Search";
-// import Code from './pages/Code/Code';
-// import { useRef } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-import img from "./assets/images/error.svg";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import DragButton from "./components/DragButton/DragButton";
+import Home from "./pages/Home/Home";
+import LessonsPage from "./pages/LessonsPage/LessonsPage";
+import SingleLesson from "./pages/SingleLesson/SingleLesson";
+import LessonVideo from "./pages/LessonVideo/LessonVideo";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Order from "./pages/Order/Order";
+import Blogs from "./pages/Blogs/Blogs";
+import BlogDetail from "./pages/Blogs/components/BlogDetail/BlogDetail";
+import SignUp from "./pages/SignUp/SignUp";
+import SignIn from "./pages/SignIn/SignIn";
+import Contact from "./pages/Contact/Contact";
+import User from "./pages/User/User";
+import Settings from "./pages/Settings/Settings";
+import Error from "./pages/Error/Error";
+import Search from "./pages/Search/Search";
+import Code from "./pages/Code/Code";
+import { useRef } from "react";
+import { Route, Routes } from "react-router-dom";
+// import img from "./assets/images/error.svg";
 function App() {
-  // const main = useRef();
+  const main = useRef();
   return (
     <>
-      {/* <Header /> */}
-      {/* <main ref={main} >
+      <Header />
+      <main ref={main}>
         <DragButton main={main} />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -49,12 +49,12 @@ function App() {
           <Route path="/code/:codeName" element={<Code />} />
           <Route path="/code/:codeName/:codeTitle" element={<Code />} />
         </Routes>
-      </main> */}
-      <div className="container text-center py-5">
+      </main>
+      {/* <div className="container text-center py-5">
         <img src={img} alt="" className='w-50 h-100' />
         <h6 className='pt-2'>Ödəniş olunduqda fəaliyətinə davam edəcək.</h6>
-      </div>
-      {/* <Footer /> */}
+      </div> */}
+      <Footer />
     </>
   );
 }
