45458527e174e1c7b66d006f7ed19f12d7b700bf
Anrsgrl <esgerlianar003@gmail.com>
2023-10-01 14:21:42 +0400

    lesson description changed/ stufflist updated

--- src/data/LessonList.js
+++ src/data/LessonList.js
@@ -5,54 +5,60 @@ import ui from "../assets/images/designer.svg";
 import design from "../assets/images/design.svg";
 import d from "../assets/images/3d.svg";
 
-
 export const lessonList = [
-    {
-        id: "1",
-        title: "Fullstack developer",
-        shortDescribe: "Html, Css, Javascript, React, MongoDB, Java & ExpressJS",
-        describe: "Siz bu kurs zamanı ilk olaraq HTML5, CSS3 (əlavə olaraq SCSS və Bootstrap) və Javascripti keçəcəksiniz. Daha sonra isə Javacscriptin frameworkü olan ReactJS ilə dərslərə davam edəcəksiniz. ReactJS müəyyən bir levelə çatdıqdan sonra siz ExpressJS, NodeJS və MongoDB öyrənəcəksiniz. Daha sonra isə müxtəlif proyektlər ilə full stack applicationlar yaradaraq bu biliklərinizi təkminləşdirəcəksiniz. Dərslər bitdikdə artıq siz sərbəst şəkildə fullstack proyektlər hazırlaya biləcəksiniz!",
-        image: fullstack,
-        path: "fullstack",
-    },
-    {
-        id: "2",
-        title: "Frontend developer",
-        shortDescribe: "Html, Css, Javascript & React",
-        describe: "Siz bu kurs zamanı ilk olaraq HTML5, CSS3 (əlavə olaraq SCSS və Bootstrap) və Javascripti keçəcəksiniz. Daha sonra isə Javacscriptin frameworkü olan ReactJS ilə dərslərə davam edəcəksiniz. Dərslər bitdikdə artıq siz istənilən websaytın interfeysini yığa biləcəksiniz!",
-        image: frontend,
-        path: "frontend",
-    },
-    {
-        id: "3",
-        title: "Backend developer",
-        shortDescribe: "Java & MySQL",
-        describe: "Siz bu kurs zamanı Java & MySQL-i irəli səviyəyə qədər öyrənəcəksiniz. Kurs bitdikdə artıq siz işləməyə hazır olacaqsınız!",
-        image: backend,
-        path: "backend",
-    },
-    {
-        id: "4",
-        title: "UI/UIX Dizayner",
-        shortDescribe: "Figma & AdobeXD",
-        describe: "Siz bu kurs zamanı Figma və AdobeXD programlarında istənilən dizaynı, tam sərbəst işləməyi öyrənəcəksiniz!",
-        image: ui,
-        path: "ui-designer",
-    },
-    {
-        id: "5",
-        title: "Interyer Dizayner",
-        shortDescribe: "Autodesk, SketchUp Pro & TurboCAD",
-        describe: "Siz bu kurs zamanı AutoDesk, SketchUp və TurboCAD kimi müsair İnteryer Dizayner programlarında işləməyi öyrənəcəksiniz!",
-        image: design,
-        path: "interior-designer",
-    },
-    {
-        id: "6",
-        title: "3D Modelləmə",
-        shortDescribe: "Blender & Cinema4D",
-        describe: "Siz bu kurs zamanı Blender və Cinema4D programlarıyla işləməyi öyrənəcək, tam sərbəst şəkildə 3d layihələr işləyə biləcəksiniz!",
-        image: d,
-        path: "3d-modelling",
-    },
-]
+  {
+    id: "1",
+    title: "Fullstack developer",
+    shortDescribe:
+      "HTML5, CSS3, Git/Github, JavaScript, React, MongoDB, NodeJS & ExpressJS",
+    describe:
+      "Siz bu kurs zamanı ilk olaraq HTML5, CSS3 (əlavə olaraq SCSS və Bootstrap) və Javascripti keçəcəksiniz. Daha sonra isə Javacscriptin librarysi olan ReactJS ilə dərslərə davam edəcəksiniz. ReactJS müəyyən bir səviyyəyə çatdıqdan sonra siz ExpressJS, NodeJS və MongoDB öyrənəcəksiniz. Daha sonra isə müxtəlif proyektlər ilə full stack applicationlar yaradaraq bu biliklərinizi təkminləşdirəcəksiniz. Dərslər bitdikdə artıq siz sərbəst şəkildə fullstack proyektlər hazırlaya biləcəksiniz!",
+    image: fullstack,
+    path: "fullstack",
+  },
+  {
+    id: "2",
+    title: "Frontend developer",
+    shortDescribe: "HTML5, CSS3, Bootstrap, Git/Github JavaScript & React",
+    describe:
+      "Siz bu kurs zamanı ilk olaraq HTML5, CSS3 (əlavə olaraq SCSS və Bootstrap) və Javascripti keçəcəksiniz. Daha sonra isə Javacscriptin librarysi olan ReactJS ilə dərslərə davam edəcəksiniz. Dərslər bitdikdə artıq siz istənilən websaytın interfeysini yığa biləcəksiniz!",
+    image: frontend,
+    path: "frontend",
+  },
+  {
+    id: "3",
+    title: "Backend developer",
+    shortDescribe: "Java, Spring, Git/Github, Oracle & MySQL",
+    describe:
+      "Siz bu kurs zamanı ilk olaraq Java dərslərinə başlayacaqsınız. Daha sonra Oracle və MySql kimi databaselər ilə işləməyi öyrənəcəksiniz. Proyektlər üzərində işləyərək Java biliklərinizi gücləndirib, daha sonra Spring frameworkünü öyrənməyə başlıyacaqsınız. Kurs bitdikdə artıq siz işləməyə hazır olacaqsınız!",
+    image: backend,
+    path: "backend",
+  },
+  {
+    id: "4",
+    title: "UI/UIX Dizayner",
+    shortDescribe: "Adobe Photoshop, Adobe Illustrator Figma & AdobeXD",
+    describe:
+      "Siz bu kurs zamanı Figma və AdobeXD programlarında istənilən dizaynı, tam sərbəst işləməyi öyrənəcəksiniz!",
+    image: ui,
+    path: "ui-designer",
+  },
+  {
+    id: "5",
+    title: "Interyer Dizayner",
+    shortDescribe: "Autodesk, SketchUp Pro & TurboCAD",
+    describe:
+      "Siz bu kurs zamanı AutoDesk, SketchUp və TurboCAD kimi müsair İnteryer Dizayner programlarında işləməyi öyrənəcəksiniz!",
+    image: design,
+    path: "interior-designer",
+  },
+  {
+    id: "6",
+    title: "3D Modelləmə",
+    shortDescribe: "Blender & Cinema4D",
+    describe:
+      "Siz bu kurs zamanı Blender və Cinema4D programlarıyla işləməyi öyrənəcək, tam sərbəst şəkildə 3d layihələr işləyə biləcəksiniz!",
+    image: d,
+    path: "3d-modelling",
+  },
+];
--- src/data/StuffList.js
+++ src/data/StuffList.js
@@ -1,4 +1,4 @@
-import boy from "../assets/images/boy.png";
+// import boy from "../assets/images/boy.png";
 import girl from "../assets/images/girl.png";
 import Nazir from "../assets/images/NazirN.jpg";
 import Anar from "../assets/images/Anar.jpg";
@@ -10,18 +10,6 @@ export const stuffList = [
     position: "Qurucu, Direktor",
     image: Nazir,
   },
-  {
-    id: "2",
-    name: "Elvin Namazov",
-    position: "Marketinq meneceri",
-    image: boy,
-  },
-  //   {
-  //     id: "3",
-  //     name: "Seymur Seferov",
-  //     position: "İnsan Resursları",
-  //     image: boy,
-  //   },
   {
     id: "4",
     name: "Anar Asgarli",
@@ -36,24 +24,5 @@ export const stuffList = [
     position: "Frontend Developer",
     image: girl,
     github: "https://github.com/lolaaskerova",
-  },
-  {
-    id: "6",
-    name: "Aynura Qaniyeva",
-    position: "Frontend Müəlliməsi",
-    image: girl,
-    github: "https://github.com/aynura12",
-  },
-  {
-    id: "7",
-    name: "Qeybulla",
-    position: "Backend Dev",
-    image: boy,
-  },
-  {
-    id: "8",
-    name: "Fuad Beybutov",
-    position: "Dizayner",
-    image: boy,
   },
 ];
