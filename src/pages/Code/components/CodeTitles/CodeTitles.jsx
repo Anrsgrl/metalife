9090208de58ea87bdc851c838add865ab646ad0c
Anrsgrl <esgerlianar003@gmail.com>
2023-10-03 19:51:03 +0400

    Mövzular text added to code titles

--- src/pages/Code/components/CodeTitles/CodeTitles.jsx
+++ src/pages/Code/components/CodeTitles/CodeTitles.jsx
@@ -37,52 +37,60 @@ const CodeTitles = ({ filteredData, url }) => {
         ))}
       </ul>
       {url && (
-        <button
-          type="button"
-          className="clean-button hamburger-menu"
-          onClick={() => openHamburger()}
-        >
-          <RxHamburgerMenu />
-        </button>
+        <div className="d-flex align-items-center">
+          <button
+            type="button"
+            className="clean-button hamburger-menu"
+            onClick={() => openHamburger()}
+          >
+            <RxHamburgerMenu />
+          </button>
+          <span className="pt-1 hamburger-menu" style={{ color: "#4A4AB5" }}>
+            Mövzular
+          </span>
+        </div>
       )}
       <AnimatePresence>
         {hamburger && (
-          <motion.aside
-            key={hamburger}
-            style={{ left: 0 }}
-            initial={{ left: -200, opacity: 0 }}
-            animate={{ left: 0, opacity: 1 }}
-            exit={{ left: -200, opacity: 0 }}
-            transition={{
-              duration: 0.3,
-              opacity: { ease: "linear" },
-            }}
-          >
-            <button
-              className="close-button clean-button"
-              onClick={() => closeHamburger()}
+          <>
+            <div className="aside-background"></div>
+            <motion.aside
+              key={hamburger}
+              style={{ left: 0 }}
+              initial={{ left: -200, opacity: 0 }}
+              animate={{ left: 0, opacity: 1 }}
+              exit={{ left: -200, opacity: 0 }}
+              transition={{
+                duration: 0.3,
+                opacity: { ease: "linear" },
+              }}
             >
-              <AiOutlineCloseCircle />
-            </button>
-            <div className="aside-content">
-              {url && <h3 className="pb-2">{url?.toUpperCase()} Kodları</h3>}
-              <ul className="code-title-hamburger custom-scroll p-0 m-0">
-                {sortedData?.map((code) => (
-                  <li onClick={() => closeHamburger()} key={code.id}>
-                    <NavLink
-                      to={`/code/${url}/${code?.title
-                        ?.toLowerCase()
-                        .split(" ")
-                        .join("-")}`}
-                      className="p-2"
-                    >
-                      {code.title}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </motion.aside>
+              <button
+                className="close-button clean-button"
+                onClick={() => closeHamburger()}
+              >
+                <AiOutlineCloseCircle />
+              </button>
+              <div className="aside-content">
+                {url && <h3 className="pb-2">{url?.toUpperCase()} Kodları</h3>}
+                <ul className="code-title-hamburger custom-scroll p-0 m-0">
+                  {sortedData?.map((code) => (
+                    <li onClick={() => closeHamburger()} key={code.id}>
+                      <NavLink
+                        to={`/code/${url}/${code?.title
+                          ?.toLowerCase()
+                          .split(" ")
+                          .join("-")}`}
+                        className="p-2"
+                      >
+                        {code.title}
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </motion.aside>
+          </>
         )}
       </AnimatePresence>
     </div>
